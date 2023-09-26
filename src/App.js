73d588f7bbb55f39bd21663e70802cd23bffbafd
bfrpaulonmios <142221823+bfrpaulonmios@users.noreply.github.com>
2023-09-26 11:03:54 +0100

    update App.js and src/index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Router, RouterProvider, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./Pages/HomePage/homePage";
 import Flower from "./Images/red-flower-frame-png-pic-vector-clipart-228173.png";
 import VideoPage from "./Pages/VideoPage/video";
@@ -8,11 +8,13 @@ import LoginPage from "./Pages/LoginPage/loginPage";
 function App() {
   return (
     <div className="body_main">
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/video-page" element={<VideoPage />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/video-page" element={<VideoPage />} />
+        </Routes>
+      </BrowserRouter>
       <img src={Flower} className="img1" draggable={false} />
       <img src={Flower} className="img2" draggable={false} />
     </div>
